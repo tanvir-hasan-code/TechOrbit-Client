65a904772a7bbbc789ab083699bf54aed0d658be
tanvir-hadan-code <tanvirx015@gmail.com>
2025-10-25 01:49:17 +0600

    feat(Axios): Axios Interceptors Error Handleing

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React from 'react';
 import useAuth from './useAuth';
+import { useNavigate } from 'react-router';
 
 const axiosSecure = axios.create({
 	baseURL: "http://localhost:5000",
@@ -9,14 +10,29 @@ const axiosSecure = axios.create({
 
 const useAxiosSecure = () => {
 
-	const { user } = useAuth();
+	const { user, logOut } = useAuth();
 	const Token = user?.accessToken;
+	const navigate = useNavigate();
 
 	axiosSecure.interceptors.request.use((config) => {
 		config.headers.Authorization = `Bearer ${Token}`
 		return config;
 	}, (error) => {
 		return Promise.reject(error)
+	})
+
+	axiosSecure.interceptors.response.use((config) => {
+		return config;
+	}, (error) => {
+		const status = error.status;
+		if (status === 503) {
+			navigate("/forbidden403")
+		} else if (status === 403) {
+			logOut().then(() => {
+				navigate("/login")
+			})
+			.catch(()=>{})
+		}
 	})
 
 
