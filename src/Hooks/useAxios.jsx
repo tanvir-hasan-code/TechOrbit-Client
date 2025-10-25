452e2dc7f56cf5a9529abf10d7cfd965035ee77e
tanvir-hadan-code <tanvirx015@gmail.com>
2025-10-25 22:05:57 +0600

    feat(URL): Replace Server URL

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React from 'react';
 
 const axiosInstance = axios.create({
-	baseURL: "http://localhost:5000",
+	baseURL: "https://tech-orbit-server-sepia.vercel.app",
 	withCredentials: true
 })
 
--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,44 +1,48 @@
-import axios from 'axios';
-import React from 'react';
-import useAuth from './useAuth';
-import { useNavigate } from 'react-router';
+import axios from "axios";
+import React from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
-	baseURL: "http://localhost:5000",
-	withCredentials: true
-})
+  baseURL: "https://tech-orbit-server-sepia.vercel.app",
+  withCredentials: true,
+});
 
 const useAxiosSecure = () => {
+  const { user, logOut } = useAuth();
+  const Token = user?.accessToken;
+  const navigate = useNavigate();
 
-	const { user, logOut } = useAuth();
-	const Token = user?.accessToken;
-	const navigate = useNavigate();
+  axiosSecure.interceptors.request.use(
+    (config) => {
+      config.headers.Authorization = `Bearer ${Token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
 
-	axiosSecure.interceptors.request.use((config) => {
-		config.headers.Authorization = `Bearer ${Token}`
-		return config;
-	}, (error) => {
-		return Promise.reject(error)
-	})
+  axiosSecure.interceptors.response.use(
+    (config) => {
+      return config;
+    },
+	  (error) => {
+		console.log(error)
+      const status = error.status;
+      if (status === 403) {
+        navigate("/forbidden403");
+      } else if (status === 401) {
+        logOut()
+          .then(() => {
+            navigate("/login");
+          })
+          .catch(() => {});
+      }
+    }
+  );
 
-	axiosSecure.interceptors.response.use((config) => {
-		return config;
-	}, (error) => {
-		const status = error.status;
-		if (status === 503) {
-			navigate("/forbidden403")
-		} else if (status === 403) {
-			logOut().then(() => {
-				navigate("/login")
-			})
-			.catch(()=>{})
-		}
-	})
-
-
-
-	return axiosSecure;
+  return axiosSecure;
 };
-
 
 export default useAxiosSecure;
